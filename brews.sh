0c055a374c55a4fd31384f1c6160b3b50a740486
Metaphox <foxisme@gmail.com>
2026-06-29 18:11:22 +0200

    revise

---
+++ brews.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+FORMULAE=$(cat << 'EOF'
+# Daily Tools
+coreutils
+neovim
+fzf
+zoxide
+git
+jq
+go
+ripgrep
+fd
+tig
+mole
+bat
+glow
+duckdb
+luajit
+pandoc
+task
+uv
+
+# Neovim Dependencies
+tree-sitter-cli
+EOF)
+
+EXTRA=$(cat << 'EOF'
+ffmpeg
+EOF)
+
+CASK=$(cat << 'EOF'
+visual-studio-code
+EOF)
+
+brew update
+brew upgrade
+
+echo "$FORMULAE" | grep -v '^\s*#' | grep -v '^\s*$' | while IFS= read -r f; do
+	brew install "$f"
+done
+
+echo "$CASK" | grep -v '^\s*#' | grep -v '^\s*$' | while IFS= read -r f; do
+	brew install --cask "$f"
+done
